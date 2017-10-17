eb8ac0f47232f66a8884a8ba71035144ab8de271
GeoffreyHW <wonggh97@gmail.com>
2017-10-16 17:42:13 -0700

    remove html extension

--- views/contact.js
+++ views/contact.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-
-
 	document.getElementById("Submit").addEventListener("click", function() {
-			var name = document.getElementById("name").value;
-			var email = document.getElementById("email").value;
-			var subject = document.getElementById("subject").value;
-			var message = document.getElementById("message").value;
+		var name = document.getElementById("name").value;
+		var email = document.getElementById("email").value;
+		var subject = document.getElementById("subject").value;
+		var message = document.getElementById("message").value;
 
-			var data = {"name" : name, "email" : email, "subject" : subject, "message" : message};
+		console.log(name, email, subject, message);
 
-			$.ajax({
-			    type: 'post',
-			    url: '/sendemail',   
-			    data: {hello : "hello"},
-			    xhrFields: {
-			        withCredentials: false
-			    },  
-			    headers: {
-			    	"data" : JSON.stringify(data)
-			    }, 
-			    success: function (data) {
-			        console.log('Success');
-			    },  
-			    error: function () {
-			        console.log('We are sorry but our servers are having an issue right now');
-			    }
-			});
+		var data = {"name" : name, "email" : email, "subject" : subject, "message" : message};
+
+		$.ajax({
+			type: 'post',
+			url: '/sendemail',   
+			data: {hello : "hello"},
+			xhrFields: {
+				withCredentials: false
+			},  
+			headers: {
+				"data" : JSON.stringify(data)
+			}, 
+			success: function (data) {
+				console.log('Success');
+			},  
+			error: function () {
+				console.log('We are sorry but our servers are having an issue right now');
+			}
 		});
+	});
 });
